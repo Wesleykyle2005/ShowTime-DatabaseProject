ddc7ecb0987a99ebe8f4cc56d11fcb3a0d0960ba
camilodv0112 <camilodonis01122005@gmail.com>
2024-11-07 08:17:30 -0600

    Cambio de prueba

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             button1.Location = new Point(291, 107);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(250, 161);
             button1.TabIndex = 0;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
